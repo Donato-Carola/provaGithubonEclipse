9ffc354f323b763e74c3b9eda6ad23b88233f314
Donato Carola <don94@live.it>
2024-07-17 19:16:25 +0200

    initial commit, github.

---
+++ src/eserciziJava/prova.java
@@ -0,0 +1,11 @@
+package eserciziJava;
+
+public class prova {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello!!, nuovo corso su java, provo collegamnento con github");
+
+	}
+
+}
